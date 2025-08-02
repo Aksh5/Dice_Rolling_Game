b4229a2d3271e25b0875358035a7cbcfa7fa7cc3
Akshada <94671043+Aksh5@users.noreply.github.com>
2025-08-02 12:03:17 +0530

    Create sample_output.py

---
+++ sample_output.py
@@ -0,0 +1,18 @@
+Roll the dice? 🎲 (y/n):  y
+You rolled: ⚁, ⚀ (Values: 2, 1)
+
+Roll the dice? 🎲 (y/n):  a
+Invalid choice! 🎲
+
+Roll the dice? 🎲 (y/n):  y
+You rolled: ⚂, ⚄ (Values: 3, 5)
+
+Roll the dice? 🎲 (y/n):  
+Invalid choice! 🎲
+
+Roll the dice? 🎲 (y/n):  y
+You rolled: ⚂, ⚂ (Values: 3, 3)
+🎊 Lucky Roll! You rolled a double! 🎊
+
+Roll the dice? 🎲 (y/n):  n
+Thanks for playing! 🎲
